ca3e267d6546bd3f860210edc1f840a5dcf62907
blessymol <roseblessy6@gmail.com>
2020-12-08 17:40:18 +0530

    basic edits

--- src/app.js
+++ src/app.js
@@ -13,6 +13,62 @@ app.use(bodyParser.json())
 
 // your code goes here
 
+app.get("/mario", async(req,res) => (
+    await marioModel.find()
+));
 
+app.get("/mario/:id", async (req,res) => {
+    const id=req.params.id;
+    try {
+        const mario=marioModel.findById(id);
+        if(mario === null) {
+            res.status(400).send({error: "Id not found"});
+        } else {
+            res.send(mario);
+        }
+    } catch(err) {
+        res.status(400).send({error: err.message});
+    }
+});
+
+app.post("/mario", async (req,res) => {
+    if((req.body.name) && (req.body.weight)) {
+        const new_mario=new marioModel(req.body);
+        await new_mario.save();
+        res.status(201).send(new_mario);
+    } else {
+        res.status(400).send({message: 'either name or weight is missing'});
+    }
+});
+
+app.patch("/mario/:id", async (req,res) => {
+    const id=req.params.id;
+    try {
+        const mario=await marioModel.findById(id);
+        if(mario===null) {
+            res.status(400).send({message: "Id not found"});
+        } else {
+            await marioModel.updateOne({_id : id}, req.body);
+            res.send(await marioModel.findById(id));
+        }
+    } catch(error) {
+        res.status(400).send({message: error.message});
+    }
+});
+
+app.delete("/mario/:id", async (req,res) => {
+    const id=req.params.id;
+    try {
+        const mario=await marioModel.findById(id);
+        if(mario===null) {
+            res.status(400).send({message: "Id not found"});
+        } else {
+            await marioModel.deleteOne({_id: id});
+            res.send({message: 'character deleted'});
+        }
+    } catch(error) {
+        res.status(400).send({message: error.message});
+    }
+});
 
 module.exports = app;
